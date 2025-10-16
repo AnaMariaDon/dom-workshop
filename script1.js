8282bab6f39c2bb6cbbd3d78dd26409736ef5e97
AnaMariaDonosoContreras <anamaria.donosocontreras@student.kyh.se>
2025-10-16 10:18:04 +0200

    del1

---
+++ script1.js
@@ -0,0 +1,33 @@
+//navigator: Dennis, Emelie
+//driver: Ana Maria
+
+const rubrik = document.createElement ("h2");
+const paragraf = document.createElement ("p");
+const ruta = document.createElement ("div");
+
+rubrik.textContent = "Min rubrik för dagen";
+rubrik.style.color = "#1e3a8a";
+
+document.body.appendChild (rubrik);
+
+
+paragraf.textContent = "Beskriv vädret idag. Kallt";
+paragraf.style.color = "#fef3c7";
+
+document.body.appendChild (paragraf);
+
+
+ruta.textContent = "Det här är en färgad ruta";
+ruta.style.backgroundColor = "#10b981";
+ruta.style.padding = "10px";
+ruta.style.width = "200px";
+
+document.body.appendChild (ruta);
+
+paragraf.textContent = "16 oktober";
+
+
+
+
+
+
